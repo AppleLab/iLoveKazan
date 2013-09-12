541f5f29bdb347831b693ea462afb7fc009a9c8b
Muzzja <user@Mac-mini-administrator-62.local>
2013-09-12 18:01:07 +0400

    Initial commit

---
+++ iLoveKazan/iLoveKazan/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iLoveKazan
+//
+//  Created by user on 12.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iLoveKazan/iLoveKazanTests/iLoveKazanTests.m
@@ -0,0 +1,34 @@
+//
+//  iLoveKazanTests.m
+//  iLoveKazanTests
+//
+//  Created by user on 12.09.13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iLoveKazanTests : XCTestCase
+
+@end
+
+@implementation iLoveKazanTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
